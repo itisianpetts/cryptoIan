f72f47dc8db414e963b58e9280a23dfdf2b57415
itisianpetts <itisianpetts@gmail.com>
2021-12-30 16:50:54 +0000

    First

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar, Welcome, Footer, Services, Transactions } from './components';
+
+const App = () => {
+  return (
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+    </div>
+  );
+};
+
+export default App;
