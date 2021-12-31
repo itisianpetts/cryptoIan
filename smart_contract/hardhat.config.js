6d888fd1d88f03f8b662687cb406ac749135d047
itisianpetts <itisianpetts@gmail.com>
2021-12-31 16:17:14 +0000

    deployed smart contract to Alchemy

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/8g1IL1pZQL7MEZONCa7t2KuwwbuLIlQk',
+      accounts: ['2960131dbc91447ddd67c0f6d662378afb0c79eb8eccf3f2882ee6bef8c051d1'],
+    },
+  },
 };
+
+// 0xD89a69ee28A45b0926687f5446d9d6bc7Eee3399
