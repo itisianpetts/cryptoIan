6cd86ae4d31566b9090d2153c2ec491fab3414bd
itisianpetts <itisianpetts@gmail.com>
2022-01-02 11:22:58 +0000

    Add Services and Dummy Data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,14 @@ import { Navbar, Welcome, Footer, Services, Transactions } from './components';
 
 const App = () => {
   return (
-    <div className='min-h-screen'>
-      <div className='gradient-bg-welcome'>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
         <Navbar />
         <Welcome />
       </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -1,9 +1,55 @@
-const Services = () => {
-  return (
-    <div>
-      <h1>Services</h1>
+import React from 'react';
+import { BsShieldFillCheck } from 'react-icons/bs';
+import { BiSearchAlt } from 'react-icons/bi';
+import { RiHeart2Fill } from 'react-icons/ri';
+
+const ServiceCard = ({ color, title, icon, subtitle }) => (
+  <div className="flex flex-row justify-start items-center white-glassmorphism p-3 mb-2 cursor-pointer hover:shadow-xl md:w-9/12">
+    <div
+      className={`w-10 h-10 rounded-full flex justify-center items-center ${color} `}
+    >
+      {icon}
     </div>
-  );
-};
+    <div className="ml-5 flex flex-col flex-1">
+      <h3 className="mt-2 text-white text-lg">{title}</h3>
+      <p className="mt-1 mb-2 text-white text-sm md:w-11/12">{subtitle}</p>
+    </div>
+  </div>
+);
+
+const Services = () => (
+  <div className="flex w-full justify-center items-center gradient-bg-services">
+    <div className="flex mf:flex-row flex-col items-center justify-between md:p-10 py-8 px-4">
+      <div className="flex-1 flex flex-col justify-start items-center">
+        <h1 className="text-white text-3xl sm:text-5xl py-2 m-10 text-gradient ">
+          Services that we
+          <br />
+          continue to improve
+        </h1>
+      </div>
+
+      <div className="flex-1 flex flex-col justify-start items-center w-9/12">
+        <ServiceCard
+          color="bg-[#2952E3]"
+          title="Security gurantee"
+          icon={<BsShieldFillCheck fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#8945F8]"
+          title="Best exchange rates"
+          icon={<BiSearchAlt fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+        <ServiceCard
+          color="bg-[#F84550]"
+          title="Fastest transactions"
+          icon={<RiHeart2Fill fontSize={21} className="text-white" />}
+          subtitle="Security is guranteed. We always maintain privacy and maintain the quality of our products"
+        />
+      </div>
+    </div>
+  </div>
+);
 
 export default Services;
